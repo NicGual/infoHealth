f8cd656930908a82acf83c893336521896ac103a
Nicolas Gualteros <gualteros.herrera.nicolas@gmail.com>
2022-01-25 16:33:06 -0500

    add signin and signup,and  change database model

--- info_health/backend/src/controllers/auth.controller.js
+++ info_health/backend/src/controllers/auth.controller.js
@@ -21,13 +21,17 @@ const login = async (req, res) => {
     
 };
 const singup = async(req, res) => {
-    const {name, email, password, confirmpassword, usertype} = req.body
+    const {identification_type,identification,name,lastname,sec_lastname,date_of_bird,gender,blood_type,
+        rh,marital_status,EPS,home_phone,mobile_phone,work_phone,address,city,department,role,email,
+        password,confirm_password,contact_name,contact_lastname,contact_sec_lastname,contact_relationship,contact_phone
+        } = req.body
+    /*const {name, email, password, confirmpassword, role} = req.body*/
     const validation = await User.findOne({email: email})
     if (password.length <4 ) {
         console.log('password must have at least 4 characters ')
         
     }
-    if (password != confirmpassword) {
+    if (password != confirm_password) {
         console.log('password does not match ')
 
     }
@@ -35,19 +39,19 @@ const singup = async(req, res) => {
         console.log('Please Write a Password')
 
     }
-    if (['patient','doctor', 'laboratorist'].includes(usertype)) {
+    if (['Paciente','Empleado', 'Medico', 'Admin'].includes(role)) {
         console.log('valid user ')
 
     }
     if (!validation) { 
 
-        const newUser =  new User ({name, 
-            email, 
-            password, 
-            usertype})
+        data={identification_type,identification,name,lastname,sec_lastname,date_of_bird,gender,blood_type,
+            rh,marital_status,EPS,home_phone,mobile_phone,work_phone,address,city,department,role,email,
+            password,contact_name,contact_lastname,contact_sec_lastname,contact_relationship,contact_phone};
+        const newUser =  new User (data)
         newUser.password = await newUser.encryptPassword(password)
         await newUser.save();
-        res.json({name, email, password, usertype})
+        res.json({name, email, password, role})
 
     }
     else {
--- info_health/backend/src/models/User.js
+++ info_health/backend/src/models/User.js
@@ -1,28 +1,118 @@
-const {Schema, model} = require('mongoose');
-const bcrypt = require ('bcrypt')
+const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt')
 
 const userSchema = new Schema({
-    name: {
+    identification_type: {
+        type: String
+    },
+    identification: {
         type: String,
         required: true,
-        trim: true,     
+        unique: true
     },
+    name: {
+        type: String,
+        required: true
+    },
+    lastname: {
+        type: String,
+        required: true
+    },
+    sec_lastname: {
+        type: String
+    },
+
+    ////////////// Información general ///////////////////
+    date_of_bird: {
+        type: String,
+        required: true,
+    },
+    gender: {
+        type: String,
+        required: true,
+    },
+    blood_type: {
+        type: String,
+        required: true,
+    },
+    rh: {
+        type: String,
+        required: true,
+    },
+    marital_status: {
+        type: String
+    },
+    EPS: {
+        type: String,
+        required: true
+    },
+
+    ////////////// Datos de localización ///////////////////
+    home_phone: {
+        type: String
+    },
+    mobile_phone: {
+        type: String
+    },
+    work_phone: {
+        type: String
+    },
+    address: {
+        type: String
+    },
+    city: {
+        type: String
+    },
+    department: {
+        type: String
+    },
+
+    ////////////// Información de la sesión ///////////////////
+    /* rol: {
+         type: mongoose.Schema.Types.ObjectId, ref: 'roles',
+         required: true
+     },  */
+
+    role: {
+        type: String,
+        required: true,
+        default: "patient",
+        enum: ["Admin", "Medico", "Empleado", "Paciente"]
+    },
+
     email: {
         type: String,
         required: true,
-        trim: true,   
+        unique: true
     },
     password: {
         type: String,
         required: true,
-        trim: true,   
     },
-    usertype: {
-        type: String,
-        required: true,
-        trim: true,   
+
+    ////////////// Información de contacto ///////////////////
+    contact_name: {
+        type: String
+    },
+    contact_lastname: {
+        type: String
+    },
+    contact_sec_lastname: {
+        type: String
+    },
+    contact_relationship: {
+        type: String
+    },
+    contact_phone: {
+        type: String
+    },
+
+    ////////////////////////////////////////////
+    last_login_date: {
+        type: Date,
+        default: Date.now()
     }
-},{timestamps: true});
+}, { timestamps: true });
 
 userSchema.methods.encryptPassword = async (password) => {
     const salt = await bcrypt.genSalt(10)
@@ -30,11 +120,11 @@ userSchema.methods.encryptPassword = async (password) => {
     return hash
 }
 
-userSchema.methods.matchPassword = async (logPassword,password) => {
-    comparison = await bcrypt.compare(logPassword,password);
+userSchema.methods.matchPassword = async (logPassword, password) => {
+    comparison = await bcrypt.compare(logPassword, password);
     return comparison;
 
 }
 
-module.exports = model('User',userSchema);
+module.exports = model('User', userSchema);
 
